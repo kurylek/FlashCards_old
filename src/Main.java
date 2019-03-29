21d796ea082039bbd9e9c98cd8247297f8da3435
kurylek <kurylek98@gmail.com>
2019-03-29 13:58:01 +0100

    Added saving/loading FlashCards, Added random oder in normal mode

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,82 @@
 /*
 TODO:
+-improve menu
 -play mode with tips
 -better way to exit/continue play mode
--save/load flascards to file
--in playNormalMode: if index==flasCardsCount set index=0, randomize randomOrder and play again
  */
 
+import java.io.*;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-    static int flashCardsCount=0;
+    static int flashCardsCount= 0;
     static FlashCard[] flashCards = new FlashCard[100];
     static Scanner input= new Scanner(System.in);
+    static String currentFlashCardsFile= "default";
+    static int firstRun=1;
+
+    public static void saveFlashCards() throws IOException{
+        PrintWriter printWriter= new PrintWriter(currentFlashCardsFile+".txt");
+
+        printWriter.println(flashCardsCount);
+        for(int i=0;i<flashCardsCount;i++){
+            printWriter.println(flashCards[i].word);
+            printWriter.println(flashCards[i].translation);
+        }
+
+        printWriter.close();
+        System.out.println(flashCardsCount+" FlashCards saved.");
+    }
+
+    public static void loadFlashCards() throws IOException{
+        clearFlashCards();
+
+        if(firstRun!=1){
+            System.out.println("Enter file name: ");
+            currentFlashCardsFile= input.next();
+        }
+        clearTerminal();
+
+        File flashCardsFile= new File(currentFlashCardsFile+".txt");
+        boolean flashCardsFileExists= flashCardsFile.exists();
+
+        if(flashCardsFileExists){
+            FileReader fileReader= new FileReader(currentFlashCardsFile+".txt");
+            BufferedReader reader= new BufferedReader(fileReader);
+
+            //reading first file line- we expect number of FlashCards
+            String tmp= reader.readLine();
+            if(tmp==null || tmp.equals("0")){
+                if(firstRun!=1){
+                    System.out.println("There is no FlashCards in these fille!");
+                }
+            }else{
+                flashCardsCount= Integer.parseInt(tmp);
+
+                for(int i=0;i<flashCardsCount;i++){
+                    String word= reader.readLine();
+                    String translation= reader.readLine();
+                    flashCards[i]= new FlashCard(word, translation);
+                }
+
+                reader.close();
+
+                System.out.println("Loaded "+flashCardsCount+" FlashCards.");
+            }
+        }else{
+            if(firstRun!=1){
+                System.out.println("There is no file with that name!");
+            }
+        }
+
+        firstRun=0;
+    }
+
+    public static void clearFlashCards(){
+        flashCardsCount= 0;
+        flashCards= new FlashCard[100];
+    }
 
     public static void addFlashCard(){
         String word;
@@ -53,27 +118,39 @@ public class Main {
     }
 
     public static  void playLearnMode(){
-        int i= 0;
-        int zeroToExit= 1;
+        int i=0;
+        String zeroToExit= "1";
         do{
             listOneFlashCard(i);
             System.out.println("Enter 0 to exit Learn Mode");
-            zeroToExit= input.nextInt();
+            zeroToExit= input.next();
             i++;
             if(i>=flashCardsCount)i=0;
-        }while(zeroToExit!=0);
+        }while(zeroToExit!="0");
+    }
+
+    public static int[] randomizeArray(int[] array){
+        Random rgen= new Random();
+        for(int i=0;i<array.length;i++){
+            int randomPosition= rgen.nextInt(array.length);
+            int tmp= array[i];
+            array[i]= array[randomPosition];
+            array[randomPosition]= tmp;
+        }
+
+        return array;
     }
 
     public static void playNormalMode(){
         int[] randomOrder= new int[flashCardsCount];
         String inputAnswer;
-        int score=0, index=0, exit=1; //exit==0 -> exit
-        for(int i=0;i<flashCardsCount;i++){
-            randomOrder[i]=i;
-            /*
-            TODO: fill randomOrder[] with values <0;flasCardsCount-1> randomly
-             */
+        int score=0, index=0, tries=0;
+        String exit= "1"; //exit==0 -> exit
+        for(int i=0;i<randomOrder.length;i++){
+            //Fill array with values 0-flashCardsCount
+            randomOrder[i]= i;
         }
+        randomOrder= randomizeArray(randomOrder);
 
         do{
             /*
@@ -83,23 +160,32 @@ public class Main {
             System.out.println("Enter translation: ");
             inputAnswer= input.next();
             inputAnswer+= input.nextLine();
+            tries++;
 
             if(flashCards[randomOrder[index]].translation.equals(inputAnswer)){
                 score++;
-                System.out.println("Good job! Your score is: "+score+"/"+(index+1));
+                System.out.println("Good job! Your score is: "+score+"/"+tries);
             }else{
-                System.out.println("Not these time.. Your score is: "+score+"/"+(index+1));
+                System.out.println("Not these time.. Your score is: "+score+"/"+tries);
                 System.out.println("Correct answer is: "+flashCards[randomOrder[index]].translation);
             }
 
             System.out.println("Enter 0 to exit.");
-            exit= input.nextInt();
-            if((index+1)>=flashCardsCount)exit=0;
-            index++;
-            if(exit==0){
-                System.out.println("It's end! Your score is: "+score+"/"+index);
+            exit= input.next();
+
+            if((index+1) >= flashCardsCount){
+                //if it shows all flashcards it'll shuffle it, and start again
+                index= 0;
+                randomOrder= randomizeArray(randomOrder);
+            }else{
+                index++;
             }
-        }while(exit!=0);
+
+            if(exit.equals("0")){
+                clearTerminal();
+                System.out.println("It's end! Your score is: "+score+"/"+tries+"\n");
+            }
+        }while(!exit.equals("0"));
     }
 
     public static void menu(){
@@ -109,6 +195,8 @@ public class Main {
         System.out.println("3. Remove FlashCard");
         System.out.println("4. Play- Learn mode");
         System.out.println("5. Play- Normal mode");
+        System.out.println("6. Save FlashCards");
+        System.out.println("7. Load FlashCards");
     }
 
     public static void clearTerminal(){
@@ -117,9 +205,11 @@ public class Main {
         }
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
         int menuOption;
         clearTerminal();
+        loadFlashCards();
+
         do{
             menu();
             menuOption= input.nextInt();
@@ -162,6 +252,14 @@ public class Main {
                     }else{
                         playNormalMode();
                     }
+                    break;
+                case 6:
+                    clearTerminal();
+                    saveFlashCards();
+                    break;
+                case 7:
+                    clearTerminal();
+                    loadFlashCards();
                     break;
                 default:
                     clearTerminal();
